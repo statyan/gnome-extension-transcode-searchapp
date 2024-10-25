77854e85d41f9f22294bcf2381ebf6b3a44e684a
Statyan <k.kubusha@gmail.com>
2024-10-25 02:33:05 -0500

    chore: prettify the code

--- src/extension.js
+++ src/extension.js
@@ -1,47 +1,47 @@
-import Gio from 'gi://Gio';
-import Shell from 'gi://Shell';
+import Gio from "gi://Gio";
+import Shell from "gi://Shell";
 // noinspection JSFileReferences
-import * as AppDisplay from 'resource:///org/gnome/shell/ui/appDisplay.js';
+import * as AppDisplay from "resource:///org/gnome/shell/ui/appDisplay.js";
 
 let originalGetInitialResultSet = null;
 
 const cyrillicToLatin = new Map([
-    ['а', 'f'],
-    ['б', ','],
-    ['в', 'd'],
-    ['г', 'u'],
-    ['д', 'l'],
-    ['е', 't'],
-    ['ё', '`'],
-    ['ж', ';'],
-    ['з', 'p'],
-    ['и', 'b'],
-    ['й', 'q'],
-    ['к', 'r'],
-    ['л', 'k'],
-    ['м', 'v'],
-    ['н', 'y'],
-    ['о', 'j'],
-    ['п', 'g'],
-    ['р', 'h'],
-    ['с', 'c'],
-    ['т', 'n'],
-    ['у', 'e'],
-    ['ы', 's'],
-    ['і', 's'],
-    ['х', '['],
-    ['ф', 'a'],
-    ['ц', 'w'],
-    ['ч', 'x'],
-    ['ш', 'i'],
-    ['щ', 'o'],
-    ['ь', 'm'],
-    ['ъ', ']'],
-    ['ї', ']'],
-    ['э', ''],
-    ['є', ''],
-    ['ю', '.'],
-    ['я', 'z']
+    ["а", "f"],
+    ["б", ","],
+    ["в", "d"],
+    ["г", "u"],
+    ["д", "l"],
+    ["е", "t"],
+    ["ё", "`"],
+    ["ж", ";"],
+    ["з", "p"],
+    ["и", "b"],
+    ["й", "q"],
+    ["к", "r"],
+    ["л", "k"],
+    ["м", "v"],
+    ["н", "y"],
+    ["о", "j"],
+    ["п", "g"],
+    ["р", "h"],
+    ["с", "c"],
+    ["т", "n"],
+    ["у", "e"],
+    ["ы", "s"],
+    ["і", "s"],
+    ["х", "["],
+    ["ф", "a"],
+    ["ц", "w"],
+    ["ч", "x"],
+    ["ш", "i"],
+    ["щ", "o"],
+    ["ь", "m"],
+    ["ъ", "]"],
+    ["ї", "]"],
+    ["э", ""],
+    ["є", ""],
+    ["ю", "."],
+    ["я", "z"],
 ]);
 
 const latinToCyrillic = new Map();
@@ -54,21 +54,36 @@ function generateInvertedDict(sourceMap, destMap) {
 
 function transcode(source, transcodeCharMap) {
     source = source.toLowerCase();
-    return [...source].map(char => transcodeCharMap.get(char) || char).join('');
+    return [...source]
+        .map((char) => transcodeCharMap.get(char) || char)
+        .join("");
 }
 
 function getResultSet(terms) {
-    let query = terms.join(' ');
+    let query = terms.join(" ");
     let groups = [];
-    const systemCommands = ['restart', 'poweroff', 'shutdown', 'reboot', 'logout'];
+    const systemCommands = [
+        "restart",
+        "poweroff",
+        "shutdown",
+        "reboot",
+        "logout",
+    ];
 
     if (systemCommands.includes(query.toLowerCase())) {
-        return originalGetInitialResultSet.call(AppDisplay.AppSearchProvider, terms);
+        return originalGetInitialResultSet.call(
+            AppDisplay.AppSearchProvider,
+            terms
+        );
     }
     try {
         groups = Gio.DesktopAppInfo.search(query);
-        groups = groups.concat(Gio.DesktopAppInfo.search(transcode(query, cyrillicToLatin)));
-        groups = groups.concat(Gio.DesktopAppInfo.search(transcode(query, latinToCyrillic)));
+        groups = groups.concat(
+            Gio.DesktopAppInfo.search(transcode(query, cyrillicToLatin))
+        );
+        groups = groups.concat(
+            Gio.DesktopAppInfo.search(transcode(query, latinToCyrillic))
+        );
     } catch (error) {
         console.error("An error occurred while searching:", error);
     }
@@ -80,9 +95,11 @@ function getResultSet(terms) {
             let app = Gio.DesktopAppInfo.new(appID);
             return app && app.should_show();
         });
-        results = results.concat(group.sort(function (a, b) {
-            return usage.compare(a, b);
-        }));
+        results = results.concat(
+            group.sort(function (a, b) {
+                return usage.compare(a, b);
+            })
+        );
     });
     return results;
 }
@@ -91,15 +108,19 @@ export default class TranscodeAppSearchExtension {
     enable() {
         if (originalGetInitialResultSet === null) {
             generateInvertedDict(cyrillicToLatin, latinToCyrillic);
-            originalGetInitialResultSet = AppDisplay.AppSearchProvider.prototype.getInitialResultSet;
-            AppDisplay.AppSearchProvider.prototype.getInitialResultSet = getResultSet;
+            originalGetInitialResultSet =
+                AppDisplay.AppSearchProvider.prototype.getInitialResultSet;
+            AppDisplay.AppSearchProvider.prototype.getInitialResultSet =
+                getResultSet;
         }
     }
 
     disable() {
         if (originalGetInitialResultSet !== null) {
-            AppDisplay.AppSearchProvider.prototype.getInitialResultSet = originalGetInitialResultSet;
+            AppDisplay.AppSearchProvider.prototype.getInitialResultSet =
+                originalGetInitialResultSet;
             originalGetInitialResultSet = null;
         }
+        latinToCyrillic.clear();
     }
 }
